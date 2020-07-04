4a8e11c1c6fcb764274b4837fb7064478e57b0e9
Desarrollo <desarrollo@MacBook-Air-de-Administrador-2.local>
2020-07-04 20:16:32 -0300

    Se hace refactor del parseo de los csv, y se guardan datos en estructura

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,58 @@
 
 using namespace std;
 
+struct real_state_ad {
+    string id;
+    string titulo;
+    string descripcion;
+    string tipodepropiedad;
+    string direccion;
+    string ciudad;
+    string provincia;
+    string antiguedad;
+    string habitaciones;
+    string garages;
+    string banos;
+    string metroscubiertos;
+    string metrostotales;
+    string idzona;
+    string lat;
+    string lng;
+    string fecha;
+    string gimnasio;
+    string usosmultiples;
+    string piscina;
+    string escuelascercanas;
+    string centroscomercialescercanos;
+    string precio;
+};
+
+void set_real_state_ad(real_state_ad *ad, string attr, unsigned int i) {
+    if (i == 0) ad->id = attr;
+    if (i == 1) ad->titulo = attr;
+    if (i == 2) ad->descripcion = attr;
+    if (i == 3) ad->tipodepropiedad = attr;
+    if (i == 4) ad->direccion = attr;
+    if (i == 5) ad->ciudad = attr;
+    if (i == 6) ad->provincia = attr;
+    if (i == 7) ad->antiguedad = attr;
+    if (i == 8) ad->habitaciones = attr;
+    if (i == 9) ad->garages = attr;
+    if (i == 10) ad->banos = attr;
+    if (i == 11) ad->metroscubiertos = attr;
+    if (i == 12) ad->metrostotales = attr;
+    if (i == 13) ad->idzona = attr;
+    if (i == 14) ad->lat = attr;
+    if (i == 15) ad->lng = attr;
+    if (i == 16) ad->fecha = attr;
+    if (i == 17) ad->gimnasio = attr;
+    if (i == 18) ad->usosmultiples = attr;
+    if (i == 19) ad->piscina = attr;
+    if (i == 20) ad->escuelascercanas = attr;
+    if (i == 21) ad->centroscomercialescercanos = attr;
+    if (i == 22) ad->precio = attr;
+}
+
 tuple<string, string> get_phrase_token(string line) {
     string token = line;
     unsigned int token_end = token.find('"');
@@ -22,12 +74,55 @@ bool is_phrase_token(string line) {
     return line[0] == '"';
 }
 
-void print_real_state_ad(vector<string> real_state_ad) {
-    string labels[23] = {"id","titulo","descripcion","tipodepropiedad","direccion","ciudad","provincia","antiguedad","habitaciones","garages","banos","metroscubiertos","metrostotales","idzona","lat","lng","fecha","gimnasio","usosmultiples","piscina","escuelascercanas","centroscomercialescercanos","precio"};
-    for (int i = 0; i < real_state_ad.size(); ++i) {
-        cout << labels[i] << "\t: " << real_state_ad[i] << endl;
+void print_real_state_ad(real_state_ad ad) {
+    cout << "Anuncio inmobiliario" << endl;
+    cout << "id: " << ad.id << endl;
+    cout << "titulo: " << ad.titulo << endl;
+    cout << "descripcion: " << ad.descripcion << endl;
+    cout << "tipodepropiedad: " << ad.tipodepropiedad << endl;
+    cout << "direccion: " << ad.direccion << endl;
+    cout << "ciudad: " << ad.ciudad << endl;
+    cout << "provincia: " << ad.provincia << endl;
+    cout << "antiguedad: " << ad.antiguedad << endl;
+    cout << "habitaciones: " << ad.habitaciones << endl;
+    cout << "garages: " << ad.garages << endl;
+    cout << "banos: " << ad.banos << endl;
+    cout << "metroscubiertos: " << ad.metroscubiertos << endl;
+    cout << "metrostotales: " << ad.metrostotales << endl;
+    cout << "idzona: " << ad.idzona << endl;
+    cout << "lat: " << ad.lat << endl;
+    cout << "lng: " << ad.lng << endl;
+    cout << "fecha: " << ad.fecha << endl;
+    cout << "gimnasio: " << ad.gimnasio << endl;
+    cout << "usosmultiples: " << ad.usosmultiples << endl;
+    cout << "piscina: " << ad.piscina << endl;
+    cout << "escuelascercanas: " << ad.escuelascercanas << endl;
+    cout << "centroscomercialescercanos: " << ad.centroscomercialescercanos << endl;
+    cout << "precio: " << ad.precio << endl << endl;
+}
+
+tuple<string, string, int> get_phrase_token_attr(string token, string attr, ifstream &file, string line) {
+    token = token.substr(1);
+    int token_end = token.find('"');
+    if (token_end == -1) {
+        attr = token;
+        while (token_end == -1) {
+            getline(file, line);
+            token = line;
+            token_end = token.find('"');
+            if (token_end != -1) {
+                token = token.substr(0, token_end);
+            }
+            attr = attr + token;
+        }
+        token = line.substr(token_end + 2);
+    } else {
+        tuple<string, string> phrase_token = get_phrase_token(token);
+        token = get<0>(phrase_token);
+        attr = get<1>(phrase_token);
     }
-    cout << endl << endl;
+
+    return make_tuple(token, attr, token_end);
 }
 
 void read_csv(string input_csv) {
@@ -49,49 +144,32 @@ void read_csv(string input_csv) {
     unsigned int token_end = 0;
     unsigned int cols_amounts = 23;
 
-    vector<string> real_state_ad(cols_amounts); 
+    vector<real_state_ad> real_state_ads; 
 
     while (!file.eof()) {
+
         getline(file, line);
         token = line;
 
         if (line.length() > 0) {
+            real_state_ad ad;
             for (int i = 0; i < cols_amounts; ++i) {
-                // cout << "token: " << token << endl;
+
                 if (is_phrase_token(token)) {
-                    token = token.substr(1);
-                    // cout << "token: " << token << endl;
-                    int token_end = token.find('"');
-                    // cout << "token_end: " << token_end << endl;
-                    if (token_end == -1) {
-                        attr = token;
-                        while (token_end == -1) {
-                            getline(file, line);
-                            token = line;
-                            // cout << "token: " << token << endl;
-                            token_end = token.find('"');
-                            if (token_end != -1) {
-                                token = token.substr(0, token_end);
-                            }
-                            attr = attr + token;
-                        }
-                        // cout << "token: " << token << endl;
-                        token = line.substr(token_end + 2);
-                        // cout << "token: " << token << endl;
-                    } else {
-                        tuple<string, string> phrase_token = get_phrase_token(token);
-                        token = get<0>(phrase_token);
-                        attr = get<1>(phrase_token);
-                    }
+                    tuple<string, string, int> phrase_token_attr_token_end = get_phrase_token_attr(token, attr, file, line);
+                    token = get<0>(phrase_token_attr_token_end);
+                    attr = get<1>(phrase_token_attr_token_end);
+                    token_end = get<2>(phrase_token_attr_token_end);
                 } else {
                     token_end = token.find(delimiter);
                     attr = token.substr(0, token_end);
                     token = token.substr(token_end + 1);
                 }
-                real_state_ad[i] = attr;
+                set_real_state_ad(&ad, attr, i);
+                real_state_ads.push_back(ad);
             }
 
-            print_real_state_ad(real_state_ad);
+            print_real_state_ad(ad);
         }
     }
 
